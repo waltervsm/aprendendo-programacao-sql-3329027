0b5efc55ace6e4d7356cbe76e24a5df9358163e2
Walter Mendonça <118997599+waltervsm@users.noreply.github.com>
2025-03-21 15:58:36 +0000

    modified:   .vscode/settings.json modified:   exercicios/2-construindo-consultas/1-operadores.sql

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,27 @@
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
+SELECT
+InvoiceId, TrackId, UnitPrice
+FROM
+invoice_items
 
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
+SELECT
+InvoiceId, TrackId, UnitPrice,
+UnitPrice + 0.78,
+UnitPrice * 0.3,
+( UnitPrice - 0.90 ) + 1.45 
+FROM
+invoice_items
 
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
-
+SELECT
+InvoiceId, TrackId, UnitPrice,
+UnitPrice + 0.78,
+UnitPrice * 0.3,
+( UnitPrice - 0.90 ) + 1.45 
+FROM
+invoice_items
+WHERE 
+InvoiceId > 20
+AND
+InvoiceId <= 30;
