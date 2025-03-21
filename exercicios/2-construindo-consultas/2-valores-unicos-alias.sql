72b076b4652c95d50c1587a1abc98eb678a16fe1
Walter Mendonça <118997599+waltervsm@users.noreply.github.com>
2025-03-21 16:05:58 +0000

    modified:   exercicios/2-construindo-consultas/2-valores-unicos-alias.sql

--- exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
+++ exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
@@ -1,7 +1,19 @@
 -- Para a tabela "customers" crie uma consulta que retorne apenas os valores únicos da coluna Country
-
+SELECT DISTINCT
+Country
+FROM  
+customers
 
 -- Na consulta anterior, adicione a coluna Company para verificar as combinações únicas entre empresas e países
-
+SELECT DISTINCT
+Country,
+Company
+FROM  
+customers
 
 -- Nesta mesma consulta, atribua um alias com título em português para cada coluna
+SELECT DISTINCT
+Country as País,
+Company as Empresa
+FROM  
+customers
