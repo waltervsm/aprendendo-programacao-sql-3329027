4fad9efc1059e8d3f4ebd774e17d54c6d39e6e04
Walter Mendonça <118997599+waltervsm@users.noreply.github.com>
2025-03-21 17:02:39 +0000

    modified:   exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,68 @@
 -- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
+SELECT DISTINCT
+CustomerId AS Cliente_ID,
+FirstName as Nome,
+LastName AS Sobrenome,
+Address as endereco
 
+FROM customers
+WHERE
+Country LIKE 'Brazil';
 
 -- Na consulta anterior, converta o sobrenome para letras maiúsculas
+SELECT DISTINCT
+CustomerId AS Cliente_ID,
+FirstName as Nome,
+UPPER (LastName) AS Sobrenome,
+Address as endereco
 
+FROM customers
+WHERE
+Country LIKE 'Brazil';
 
 -- Na consulta anterior, converta o nome para letras minúsculas
+SELECT DISTINCT
+CustomerId AS Cliente_ID,
+LOWER (FirstName) as Nome,
+LastName AS Sobrenome,
+Address as endereco
 
+FROM customers
+WHERE
+Country LIKE 'Brazil';
 
 -- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
+SELECT DISTINCT
+CustomerId AS Cliente_ID,
+FirstName as Nome,
+LastName AS Sobrenome,
+Address as endereco,
+FirstName || ' ' || LastName AS Nome_completo
 
+FROM customers
+WHERE
+Country LIKE 'Brazil';
 
 -- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
+SELECT DISTINCT
+CustomerId AS Cliente_ID,
+FirstName as Nome,
+LastName AS Sobrenome,
+FirstName || ' ' || LastName AS Nome_completo,
+REPLACE (Address, 'Av.', 'Avenida') AS endereco
 
+FROM customers
+WHERE
+Country LIKE 'Brazil';
 
 -- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+SELECT DISTINCT
+CustomerId AS Cliente_ID,
+FirstName as Nome,
+LastName AS Sobrenome,
+FirstName || ' ' || LastName AS Nome_completo,
+REPLACE (Address, 'Av.', 'Avenida') AS endereco
+
+FROM customers
+WHERE
+Country LIKE 'Bra%';
